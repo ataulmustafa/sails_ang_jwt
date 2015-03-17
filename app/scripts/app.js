23a9e08dc260b1bb3f76b39dc0bb1e83e28b14ea
ataulmustafa <ata.mustafa89@gmail.com>
2015-03-18 01:54:32 +0500

    Enviroment Setup
    Installation of Yeoman, GIT, bower, grunt, angular generator

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sailsAngJwtApp
+ * @description
+ * # sailsAngJwtApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sailsAngJwtApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sailsAngJwtApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sailsAngJwtApp
+ */
+angular.module('sailsAngJwtApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
