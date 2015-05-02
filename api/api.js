face662255839438fc426003b0c58b89a83381fb
ataulmustafa <ata.mustafa89@gmail.com>
2015-05-02 17:29:40 +0500

    Create and Send token with Register

--- api/api.js
+++ api/api.js
@@ -25,19 +25,30 @@ app.post('/register', function(req, res){
 		email: user.email,
 		password: user.password
 	});
+    
+    var payload = {
+        issure: req.hostname,
+        subject: user._id
+    }
+    
+    var token = jwt.encode(payload, "mesecretkey");
 
 	newUser.save(function(err){
 //         toJSON function is used to hide password in User.js
-		res.status(200).send(newUser.toJSON());
+		res.status(200).send({
+            user: newUser.toJSON(), 
+            token: token
+        });
 	})
 })
 
 mongoose.connect('mongodb://localhost/psjwt');
 
-console.log(jwt.encode('hi', 'mysecret'));
+//Testing jwt with test data
+//console.log(jwt.encode('hi', 'mysecret'));
 
 
 // Express app listening
-//var server = app.listen(3000, function(){
-//	console.log('api listening on ', server.address().port);
-//})
+var server = app.listen(3000, function(){
+	console.log('api listening on ', server.address().port);
+});
